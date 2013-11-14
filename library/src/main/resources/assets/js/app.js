7293fc6e3829cdfa507d02c4306cd0814c844c38
HashmiShaik <itsmehashu@gmail.com>
2013-11-14 14:35:52 -0800

    Made some Final changes.

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,17 +1,27 @@
 $(":button").click(function() {
-	var isbnid = this.id;
-	var targeturl = 'http://localhost:8001/library/v1/books/'+isbnid+'?status=lost';
-	var buttonisbnid = ':button#'+isbnid;
-	
-	$.ajax({
-		  url: targeturl,
-		  type: 'PUT',
-		  success: function()
-		  {
-			  alert('About to report lost on isbnid ' + isbnid);
-			  location.reload();
-		  }
-		});
-	
-	$(buttonisbnid).attr("disabled",true);
+        var isbnid = this.id;
+        var targeturl = 'http://localhost:8001/library/v1/books/'+isbnid+'?status=lost';
+        var buttonisbnid = ':button#'+isbnid;
+        $.ajax({
+                 url: targeturl,
+                 type: 'PUT',
+                 success: function(data) {
+                         alert('About to report lost on isbnid ' + isbnid);
+                         location.reload();
+                 }
+                });
+        $(buttonisbnid).attr("disabled",true);
+});
+
+
+$(document).ready(function() {
+        var books = document.getElementById("books");
+        for (var i = 0, row; row = books.rows[i]; i++) {
+                var bookStatus=books.rows[i].cells[4].innerHTML;
+                if (bookStatus=="lost")
+                {
+                        var newisbnid=books.rows[i].cells[0].innerHTML;
+                        document.getElementById(newisbnid).disabled = true;
+                }
+        }
 });
